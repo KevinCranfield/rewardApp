d375fdda11aad4601b15ab701584369da53d467d
Kevin Cranfield <gills4prem@gmail.com>
2026-03-28 17:31:17 +0000

    clean project + sync server version

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#0$8$9x94@1xj%(pe_z2fxg00wo^b&um&am$lmbc4ko783w3w7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["134.209.18.202"]
+ALLOWED_HOSTS = ["rewardapp.live", "www.rewardapp.live", "134.209.18.202"]
 
 
 # Application definition
@@ -120,4 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
