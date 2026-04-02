8e81807575cd41a5915f2c1d73805bd72f7e36d8
Kevin Cranfield <gills4prem@kevins-air.home>
2026-04-02 08:52:15 +0100

    updated reset passwords

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 # TEST LINE again 
 from pathlib import Path
 import os
+
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -133,6 +138,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'cranfieldgames@gmail.com'       # your Gmail address
-EMAIL_HOST_PASSWORD = 'mqkd nsjn mamz xenx'  # Gmail App Password
-DEFAULT_FROM_EMAIL = 'Reward Game <cranfieldgames@gmail.com>'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = f'Reward Game <{os.environ.get("EMAIL_HOST_USER")}>'
