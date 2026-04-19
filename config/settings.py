006acd33e41d526e512371bf067588174162dc74
Kevin Cranfield <gills4prem@kevins-air.home>
2026-04-19 22:11:51 +0100

    updated login redirect error

--- config/settings.py
+++ config/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-LOGIN_URL = "/enter-pin/"
+LOGIN_URL = "/login/"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
